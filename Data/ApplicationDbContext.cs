099e77a5fb912bebaf8bbb0d1bd587a497986c9e
darynazar <heydaro.std00@gmail.com>
2024-10-28 11:41:12 +0330

    add the user Index.cshtml with controller for user manager

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 using Test.Models;
 
 namespace Test.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-        public DbSet<User> User { get; set; }
     }
 }
