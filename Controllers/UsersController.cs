7f6cb386b80f769246dcc8f4d98dbd559641baf0
darynazar <heydaro.std00@gmail.com>
2024-10-28 12:49:01 +0330

    add the user Edit, Detail and Delete.cshtml with controller for user manager

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -55,6 +55,106 @@ namespace Test.Controllers
             }
             return View(user);
         }
+          // GET: Users/Details/5
+            public async Task<IActionResult> Details(string id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var user = await _userManager.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                return View(user);
+            }
+
+            // GET: Users/Edit/5
+            public async Task<IActionResult> Edit(string id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var user = await _userManager.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                return View(user);
+            }
+
+            // POST: Users/Edit/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,Email")] IdentityUser user)
+            {
+                if (id != user.Id)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        var existingUser = await _userManager.FindByIdAsync(id);
+                        existingUser.UserName = user.UserName;
+                        existingUser.Email = user.Email;
+
+                        var result = await _userManager.UpdateAsync(existingUser);
+                        if (result.Succeeded)
+                        {
+                            return RedirectToAction(nameof(Index));
+                        }
+
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        // Handle exception
+                    }
+                }
+                return View(user);
+            }
+
+            // GET: Users/Delete/5
+            public async Task<IActionResult> Delete(string id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var user = await _userManager.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                return View(user);
+            }
+
+            // POST: Users/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> DeleteConfirmed(string id)
+            {
+                var user = await _userManager.FindByIdAsync(id);
+                if (user != null)
+                {
+                    await _userManager.DeleteAsync(user);
+                }
+                return RedirectToAction(nameof(Index));
+            }
     }
 }
 
