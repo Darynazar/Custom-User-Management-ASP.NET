7eb617d50d2676d996aa36b98fc215c003e3540c
darynazar <heydaro.std00@gmail.com>
2024-11-06 08:46:38 +0330

    add user_category

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -42,3 +42,59 @@ namespace Test.Data
         public DbSet<Test.Models.Category> Category { get; set; } = default!;
     }
 }
+
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+//using Test.Data.ModelPropert;
+//using Test.Models;
+//
+//namespace Test.Data
+//{
+//    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+//    {
+//        public DbSet<Permission> Permissions { get; set; }
+//        public DbSet<RolePermission> RolePermissions { get; set; }
+//        public DbSet<Category> Categories { get; set; }
+//        public DbSet<Letter> Letters { get; set; }
+//        public DbSet<UserCategory> UserCategories { get; set; }
+//
+//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//            : base(options)
+//        {
+//        }
+//
+//        protected override void OnModelCreating(ModelBuilder builder)
+//        {
+//            base.OnModelCreating(builder);
+//
+//            // Many-to-many relationship setup between User and Category
+//            builder.Entity<UserCategory>()
+//                .HasKey(uc => new { uc.UserId, uc.CategoryId });
+//
+//            builder.Entity<UserCategory>()
+//                .HasOne(uc => uc.ApplicationUser)
+//                .WithMany(u => u.UserCategories)
+//                .HasForeignKey(uc => uc.UserId);
+//
+//            builder.Entity<UserCategory>()
+//                .HasOne(uc => uc.Category)
+//                .WithMany(c => c.UserCategories)
+//                .HasForeignKey(uc => uc.CategoryId);
+//
+//            // RolePermission configuration
+//            builder.Entity<RolePermission>()
+//                .HasKey(rp => new { rp.RoleId, rp.PermissionId });
+//
+//            builder.Entity<RolePermission>()
+//                .HasOne(rp => rp.Role)
+//                .WithMany()
+//                .HasForeignKey(rp => rp.RoleId);
+//
+//            builder.Entity<RolePermission>()
+//                .HasOne(rp => rp.Permission)
+//                .WithMany()
+//                .HasForeignKey(rp => rp.PermissionId);
+//        }
+//    }
+//
+//}
